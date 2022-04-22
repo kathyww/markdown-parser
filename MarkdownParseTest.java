6e1efe4797f6a18c8d6bc928fce02a685a8910d6
cmy0357 <mic015@ucsd.edu>
2022-04-22 14:49:19 -0700

    Complete part4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,29 @@
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void getLinksTest() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        //ArrayList<String> expected = List.of("https://something.com", "some-page.html");
+
+	    assertEquals(List.of("https://something.com", "some-thing.html"), links);
+
+
     }
 }
 
