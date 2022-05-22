65eada0cc5a9c790a3bcd82b2bd0784bfa01c059
kathyww <103288344+kathyww@users.noreply.github.com>
2022-05-22 00:18:34 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,90 +10,47 @@ import java.util.Map;
 
 public class MarkdownParse {
 
-    static int findCloseParen(String markdown, int openParen) {
-        int closeParen = openParen + 1;
-        int openParenCount = 1;
-        int lastCloseParenIndex = markdown.indexOf(")", openParen);
-        while (openParenCount > 0) {
-            if (markdown.charAt(closeParen) == '(') {
-                openParenCount++;
-            } else if (markdown.charAt(closeParen) == ')') {
-                openParenCount--;
-                lastCloseParenIndex = closeParen;
-            }
-            closeParen++;
-        }
-        return lastCloseParenIndex;
-    }
-    public static Map<String, List<String>> getLinks(File directory) throws IOException {
-        Map<String, List<String>> result = new HashMap<>();
-        if(directory.isDirectory()) {
-            for(File f: directory.listFiles()) {
-                result.putAll(getLinks(f));
-            }
-            return result;
-        }
-        else {
-            Path p = directory.toPath();
-            int lastDot = p.toString().lastIndexOf(".");
-            if(lastDot == -1 || !p.toString().substring(lastDot).equals(".md")) {
-                return null;
-            }
-            ArrayList<String> links = getLinks(Files.readString(p));
-            result.put(directory.getPath(), links);
-            return result;
-        }
-    }
-
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
+
+        if (! markdown.contains("[") || ! markdown.contains("]")) {
+            return toReturn;
+        }
+
+        if (! markdown.contains("(") || ! markdown.contains(")")) {
+            return toReturn;
+        }
+
+        if (markdown.contains("![")) {
+            return toReturn;
+        }
+
+        // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCodeBlock = markdown.indexOf(System.lineSeparator() + "```");
-            if(nextCodeBlock < nextOpenBracket && nextCodeBlock!=-1 ) {
-                int endOfCodeBlock = markdown.indexOf(System.lineSeparator() + "```");
-                currentIndex = endOfCodeBlock + 1;
-                continue;
-            }
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
 
-            // System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-
-            // The close paren we need may not be the next one in the file
-            // Track opening parens and find matching close paren
-            int closeParen = findCloseParen(markdown, openParen);
-            
-            if(nextOpenBracket == -1 || nextCloseBracket == -1
-                  || closeParen == -1 || openParen == -1) {
-                return toReturn;
-            }
-            if(nextOpenBracket!=0 && markdown.charAt(nextOpenBracket-1)=='!'){
-                currentIndex = closeParen + 1;
-                continue;
-            }
-            if(nextCloseBracket != openParen-1){
-                currentIndex = closeParen + 1;
-                continue;
-            }
-            String potentialLink = markdown.substring(openParen + 1, closeParen).trim();
-            if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\n") == -1) {
-                toReturn.add(potentialLink);
-                currentIndex = closeParen + 1;
-            }
-            else {
-                currentIndex = currentIndex + 1;
-            }
+            if (! markdown.substring(currentIndex).contains("[") 
+                || ! markdown.substring(currentIndex).contains("]")
+                || ! markdown.substring(currentIndex).contains("(")
+                || ! markdown.substring(currentIndex).contains("[")) {
+                    break;
+                }
         }
+
         return toReturn;
     }
+
+
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+	    System.out.println(links);
     }
 }
