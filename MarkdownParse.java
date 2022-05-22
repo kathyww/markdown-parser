07710551d20d9852b0d476bafeace4859949611c
kathyww <kes.wen404@gmail.com>
2022-05-22 01:05:40 -0700

    2

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
+
 
 public class MarkdownParse {
 
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,22 +1,17 @@
 import static org.junit.Assert.*;
 
-
-import java.util.ArrayList;
-import java.util.List;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.*;
-public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
-    }
 
+public class MarkdownParseTest {
+    
     @Test
     public void getLinksTest() throws IOException{
-        Path fileName = Path.of("test-file.md");
+        Path fileName = Path.of("test-file1.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
 	    assertEquals(List.of("https://something.com", "some-thing.html"), links);
